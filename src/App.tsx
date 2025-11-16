bc5e9760aef2bea36a9a23e2220f2fc77d80921d
prophet-bestman <bestiproph@gmail.com>
2025-11-16 16:38:36 +0100

    feat: integrate ToastContainer for notifications
    - Add react-toastify ToastContainer to App
    - Configure toast position and behavior
    - Enable undo notifications with 5-second timeout
    - Add success/error/info toast support
    - Import required CSS for toast styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,32 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import { BoardPage } from './pages/BoardPage';
 import { IssueDetailPage } from './pages/IssueDetailPage';
 import { SettingsPage } from './pages/SettingsPage';
 import { Navigation } from './components/Navigation';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const App = () => {
-
   return (
-      <Router>
-        <Navigation />
-        <Routes>
-          <Route path="/board" element={<BoardPage />} />
-          <Route path="/issue/:id" element={<IssueDetailPage />} />
-          <Route path="/settings" element={<SettingsPage />} />
-          <Route path="*" element={<Navigate to="/board" />} />
-        </Routes>
-      </Router>
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/board" element={<BoardPage />} />
+        <Route path="/issue/:id" element={<IssueDetailPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="*" element={<Navigate to="/board" />} />
+      </Routes>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </Router>
   );
-}
+};
